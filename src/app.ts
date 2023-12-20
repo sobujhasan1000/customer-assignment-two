3842c4d1db1e4c19389ad8562485aacbdfdc7c4d
unknown <sobujhasan1000@gmail.com>
2023-12-20 11:43:29 +0600

    final issue solved

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 // application routers
-app.use('/v1/customers', customerRoutes);
+app.use('/api', customerRoutes);
 
 const getControler = (req: Request, res: Response) => {
   res.send('Hello World! hi');
--- src/app/modules/customer/customer.route.ts
+++ src/app/modules/customer/customer.route.ts
@@ -3,27 +3,21 @@ import { customerControlar } from './customer.controlar';
 
 const router = express.Router();
 
-router.post('/api/users', customerControlar.createCustomer);
+router.post('/users', customerControlar.createCustomer);
 
-router.get('/api/users', customerControlar.getAllcustomer);
+router.get('/users', customerControlar.getAllcustomer);
 
-router.get('/api/users/:userId', customerControlar.getSpecifiqcustomer);
+router.get('/users/:userId', customerControlar.getSpecifiqcustomer);
 
-router.delete('/api/users/:userId', customerControlar.deletSpecifiqcustomer);
+router.delete('/users/:userId', customerControlar.deletSpecifiqcustomer);
 
-router.put('/api/users/:userId', customerControlar.updateSpecifiqcustomer);
+router.put('/users/:userId', customerControlar.updateSpecifiqcustomer);
 
 router.get(
-  '/api/users/:userId/orders/total-price',
+  '/users/:userId/orders/total-price',
   customerControlar.totalSpecifiqcustomerOrderPrice,
 );
-router.get(
-  '/api/users/:userId/orders',
-  customerControlar.getSpecifiqcustomerOrder,
-);
-router.put(
-  '/api/users/:userId/orders',
-  customerControlar.addNewProductCustomer,
-);
+router.get('/users/:userId/orders', customerControlar.getSpecifiqcustomerOrder);
+router.put('/users/:userId/orders', customerControlar.addNewProductCustomer);
 
 export const customerRoutes = router;
